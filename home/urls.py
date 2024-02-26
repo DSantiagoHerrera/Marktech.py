6038eb0b5921dfdd4e35496a7a2352b964401ddd
jaarrubla2 <126577040+jaarrubla2@users.noreply.github.com>
2024-02-26 11:20:34 -0500

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('venta', ventaView, name='venta'),
     path('inventario', inventarioView, name='inventario'),
     path('pqrs', pqrsView, name='pqrs' ),
+    path('registrarpqrs/', registrarpqrs),
     path('productos', productosView, name='productos'),
     path('registrarProducto/', registrarProducto),
     path('edicionProducto/<codigo>', edicionProducto),
