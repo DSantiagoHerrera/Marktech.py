9d2729eece757cc2bcc210bac9ae67bead8d09ea
Jccristancho <126576642+Jccristancho@users.noreply.github.com>
2024-03-04 11:32:33 -0500

    Update models.py

--- home/models.py
+++ home/models.py
@@ -23,9 +23,20 @@ class Pqrs(models.Model):
         return texto.format(self.nombre, self.tipoPqrs)
 
 class Venta(models.Model):
-    codigo = models.CharField(primary_key=True, max_length=6)
+    codigo = models.AutoField(primary_key=True, max_length=6)
     productos = models.ManyToManyField(Producto) 
     totalVenta = models.CharField(max_length=51)
+
+    def calcular_total_venta(self):
+        total = 0
+        for producto in self.productos.all():
+            total += producto.precio
+        self.totalVenta = total
+        self.save()
+
+    def save(self, *args, **kwargs):
+        self.calcular_total_venta()
+        super().save(*args, **kwargs)
     
     def __str__(self):
         texto = "{0} ({1})"
